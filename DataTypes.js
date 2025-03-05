329cf4f0b553c1bb63ccf48b90fff6af61fb63ea
vinay-chindam <122191158+vinay-chindam@users.noreply.github.com>
2025-03-05 17:50:07 +0000

    "objects revisit"

--- DataTypes.js
+++ DataTypes.js
@@ -26,3 +26,28 @@ Functions
 const s1=Symbol('56')
 const s2=Symbol('56')
 console.log(s1===s2);
+
+
+
+
+const user = {
+  id: 1,
+  name: "John Doe",
+  email: "johndoe@example.com",
+  age: 25,
+  isActive: true
+};
+
+console.log(user);
+
+user.f1=function(){
+    console.log("I am function insede an object");
+    
+}
+/* console.log(user);
+user.f1()
+ */
+
+
+const person=new Object()
+const person2={}
