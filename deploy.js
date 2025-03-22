79ebce428f640e827e330e7c2bf2314b5e77e117
vinay-chindam <122191158+vinay-chindam@users.noreply.github.com>
2025-03-22 13:36:37 +0000

    "deploy urls committed "

---
+++ deploy.js
@@ -0,0 +1,17 @@
+/*
+
+mongodb
+mongodb+srv://vinaychindam1728:mongopassword@cluster0.wqf6xom.mongodb.net/foodapp
+
+ admin
+
+https://tastygoadmin.netlify.app/
+
+backend
+
+https://tasty-go-backend.onrender.com
+
+frontend
+https://tasty-go-frontend.onrender.com
+
+*/ 
