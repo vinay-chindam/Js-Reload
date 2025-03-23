2404cf572bf1b9556986db10f3d8d08058196b21
vinay-chindam <122191158+vinay-chindam@users.noreply.github.com>
2025-03-23 09:28:21 +0530

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -3,15 +3,15 @@
 mongodb
 mongodb+srv://vinaychindam1728:mongopassword@cluster0.wqf6xom.mongodb.net/foodapp
 
- admin
-
-https://tastygoadmin.netlify.app/
 
 backend
 
 https://tasty-go-backend.onrender.com
 
+admin
+
+https://tasty-go-admin.onrender.com
+
 frontend
 https://tasty-go-frontend.onrender.com
-
 */ 
