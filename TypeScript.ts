b5a492d2fdc8b95f960f8feff281887d983e2330
vinay-chindam <122191158+vinay-chindam@users.noreply.github.com>
2025-07-08 15:38:58 +0530

    Create TypeScript.ts

---
+++ TypeScript.ts
@@ -0,0 +1,28 @@
+Data Types in Typescript:
+
+number
+string 
+boolean
+any 
+
+
+
+const a:number=10
+const b:number
+b=20
+
+functions:
+
+function addTwo(a:number):number{
+
+  return a+2
+}
+
+addTwo(5)
+
+const func=(num:number):number=>{
+  return num+3
+}
+
+Objects in TS:
+
