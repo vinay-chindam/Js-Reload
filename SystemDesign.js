9771dfd6c30a959fa258e0b8a7d180a9ac0d528b
vinay-chindam <122191158+vinay-chindam@users.noreply.github.com>
2025-09-10 21:29:41 +0530

    Create SystemDesign.js

---
+++ SystemDesign.js
@@ -0,0 +1,50 @@
+Key Concepts:
+
+    Scalability Vertical Scaling (adding more resources to same machine)
+                Horizontal Scaling (adding more machines)
+  Performance:
+    Latency
+    Throughput
+  
+  Availability
+  Relaiablity
+  Consistency
+Buildind Blocks:
+
+  Application servers
+  Load Balancers
+  Databases
+  Caching
+  Message queue
+  Storage
+  Proxy Server
+  CDN (content delivery network)
+
+
+API Design Styles:
+
+Rest (Representational state transfer)
+GraphQL
+
+
+
+Load Balancing:
+
+Static Load Balancing (Round robin, weighted round robin,IP hash)
+Dynamic Load Balancing (Least connections, weighted least connections, Least response time)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
