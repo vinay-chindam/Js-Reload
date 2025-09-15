c3f52b7385d4bc766d2485f243e083af7cb6682f
vinay-chindam <122191158+vinay-chindam@users.noreply.github.com>
2025-09-15 17:05:22 +0530

    Create Springboot.js

---
+++ Springboot.js
@@ -0,0 +1,15 @@
+
+Inversion of control Design principle
+
+Dependency Injection  Design Pattern
+Constructor Injection
+Setter Injection
+Field Injection
+
+Annotations:
+
+The @Component annotation in Spring is a core stereotype annotation used to mark a Java class as a Spring-managed bean. 
+  When Spring performs component scanning, it automatically detects classes annotated with @Component and registers them in the ApplicationContext, 
+  making them eligible for dependency injection
+
+
